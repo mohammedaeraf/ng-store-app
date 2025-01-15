84a99487dacaffb4da9b0c15ffbcfec0060226b0
Mohammed Aeraf <aeraf@outlook.com>
2025-01-15 17:23:49 +0530

    simplified generation of pages array

--- src/app/view-products/view-products.component.ts
+++ src/app/view-products/view-products.component.ts
@@ -21,7 +21,6 @@ export class ViewProductsComponent {
   currentPage: number = 1;
   pageSize: number = 5;
 
-
   // Injecting ProductService and Router
   constructor(private productService: ProductService, private router: Router) {
     this.title = 'Products List';
@@ -29,7 +28,6 @@ export class ViewProductsComponent {
     this.filteredProductsList = [];
     this.paginatedProductsList = [];
   }
-
 
   // ngOnInit is a lifecycle hook called by Angular to indicate that Angular is done creating the component
   ngOnInit() {
@@ -47,12 +45,15 @@ export class ViewProductsComponent {
   updatePaginatedProductsList(): void {
     const startIndex = (this.currentPage - 1) * this.pageSize;
     const endIndex = startIndex + this.pageSize;
-    this.paginatedProductsList = this.filteredProductsList.slice(startIndex, endIndex);
+    this.paginatedProductsList = this.filteredProductsList.slice(
+      startIndex,
+      endIndex
+    );
   }
 
   onSearch(): void {
     if (this.searchQuery) {
-      this.filteredProductsList = this.productsList.filter(product =>
+      this.filteredProductsList = this.productsList.filter((product) =>
         product.title.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
     } else {
@@ -61,11 +62,18 @@ export class ViewProductsComponent {
     this.currentPage = 1;
     this.updatePaginatedProductsList();
   }
-  
+
   get totalPages(): number[] {
-    const totalPages = Math.ceil(this.filteredProductsList.length / this.pageSize);
-    return Array.from({ length: totalPages }, (_, i) => i + 1);
+    const totalPages = Math.ceil(
+      this.filteredProductsList.length / this.pageSize
+    );
+    const pages: number[] = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pages.push(i);
+    }
+    return pages;
   }
+  
   changePage(page: number): void {
     this.currentPage = page;
     this.updatePaginatedProductsList();
@@ -80,7 +88,9 @@ export class ViewProductsComponent {
   }
 
   deleteProduct(productId: number): void {
-    const confirmation = confirm('Are you sure you want to delete this product?');
+    const confirmation = confirm(
+      'Are you sure you want to delete this product?'
+    );
     if (confirmation) {
       this.productService.deleteProduct(productId).subscribe(() => {
         this.loadProducts();
@@ -91,6 +101,4 @@ export class ViewProductsComponent {
   editProduct(productId: number): void {
     this.router.navigate(['/edit-product', productId]);
   }
-
-  
 }
